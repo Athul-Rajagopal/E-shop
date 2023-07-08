8f23991925e2caa86ab7e54471a5021253eece0b
Athul Rajagopalan P <athulrajagopalan88@gmail.com>
2023-07-08 22:01:26 +0530

    product management, and catrgoey management were completed on admin side

--- eshop/admin_panel/views.py
+++ eshop/admin_panel/views.py
@@ -7,6 +7,8 @@ from store.models import CategoryTable, ProductTable, ProductVariant, Brands, Si
 
 # Create your views here.
 def admin_login(request):
+    if request.user.is_superuser and request.user.is_authenticated:
+        return render(request, 'admin_panel/adminHome.html')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['pass1']
@@ -85,10 +87,19 @@ def add_category(request):
             return redirect('category')
 
 
-def dlt_category(request, id):
+def disable_category(request, id):
     if request.user.is_superuser:
         cat = CategoryTable.objects.get(id=id)
-        cat.delete()
+        cat.is_active = False
+        cat.save()
+        return redirect('category')
+
+
+def enable_category(request, id):
+    if request.user.is_superuser:
+        cat = CategoryTable.objects.get(id=id)
+        cat.is_active = True
+        cat.save()
         return redirect('category')
 
 
@@ -163,6 +174,42 @@ def add_product(request):
         return render(request, 'admin_panel/add_product.html', context)
 
 
+def edit_product(request, product_id):
+    if request.user.is_superuser:
+        product = ProductTable.objects.get(id=product_id)
+        catagories = CategoryTable.objects.all()
+        brands = Brands.objects.all()
+        sizes = Size.objects.all()
+
+        context = {
+            'brands': brands,
+            'catagories': catagories,
+            'sizes': sizes,
+            'product': product
+        }
+
+        if request.method == "POST":
+            product_name = request.POST['product_name']
+            category = request.POST['category']
+            brand = request.POST['brand']
+            description = request.POST['description']
+
+            category_id = CategoryTable.objects.get(name=category)
+            brand_id = Brands.objects.get(name=brand)
+
+            product.name = product_name
+            product.category = category_id
+            product.brandName = brand_id
+            product.description = description
+
+            product.save()
+            return redirect('products')
+
+        return render(request, 'admin_panel/edit-product.html', context)
+    else:
+        return redirect('admin_home')
+
+
 def add_variant(request, product_id):
     if request.user.is_superuser:
         if request.method == 'POST':
@@ -188,6 +235,8 @@ def add_variant(request, product_id):
             'product_id': product_id
         }
         return render(request, 'admin_panel/add-variant.html', context)
+    else:
+        return redirect('admin_home')
 
 
 def disable_variant(request, variant_id):
@@ -220,3 +269,51 @@ def enable_product(request, product_id):
         product.is_active = True
         product.save()
         return redirect('products')
+
+
+def edit_category(request, category_id):
+    if request.user.is_superuser:
+        category = CategoryTable.objects.get(id=category_id)
+
+        if request.method == 'POST':
+            category_name = request.POST['category_name']
+            image = request.FILES['image']
+
+            category.name = category_name
+            category.image = image
+            category.save()
+
+            return redirect('category')
+
+        return render(request, 'admin_panel/edit-category.html', {'category': category})
+    else:
+        return redirect('admin_home')
+
+
+def edit_variant(request, variant_id):
+    if request.user.is_superuser:
+        variant = ProductVariant.objects.get(id=variant_id)
+
+        if request.method == 'POST':
+            stock = request.POST['stock']
+            size = request.POST['size']
+            price = request.POST['price']
+            variant_image = request.FILES['variant_image']
+            variant_images = request.FILES.getlist('variant_images')
+
+            size_id = Size.objects.get(size=size)
+
+            variant.stock = stock
+            variant.size = size_id
+            variant.price = price
+            variant.display_image = variant_image
+            # variant.save()
+            VariantImage.objects.filter(product_variant=variant).delete()
+
+            for image in variant_images:
+                variant_image = VariantImage.objects.create(product_variant=variant, display_image=image)
+                variant_image.save()
+
+            return redirect('products')
+
+        return render(request, 'admin_panel/edit-variant.html', {'variant': variant})
